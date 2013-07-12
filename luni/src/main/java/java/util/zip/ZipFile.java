multi-os-engine/libcore

Don't install APK files with unsupported General Purpose Bit Flag bits.
Bug: 8476102
Change-Id: I764c2aec944558129c8ac04ee0593fb3de82870e

luni/src/main/java/java/util/zip/ZipFile.java
@@ -68,6 +68,12 @@ public class ZipFile implements ZipConstants {
     static final int GPBF_UTF8_FLAG = 1 << 11;
 
     /**
+     * Supported General Purpose Bit Flags Mask.
+     * Bit mask of supported GPBF bits.
+     */
+    static final int GPBF_SUPPORTED_MASK = GPBF_DATA_DESCRIPTOR_FLAG | GPBF_UTF8_FLAG;
+
+    /**
      * Open zip file for reading.
      */
     public static final int OPEN_READ = 1;
@@ -242,11 +248,19 @@ public class ZipFile implements ZipConstants {
         RandomAccessFile localRaf = raf;
         synchronized (localRaf) {
             // We don't know the entry data's start position. All we have is the
-            // position of the entry's local header. At position 28 we find the
-            // length of the extra data. In some cases this length differs from
-            // the one coming in the central header.
-            RAFStream rafStream = new RAFStream(localRaf, entry.localHeaderRelOffset + 28);
+            // position of the entry's local header. At position 6 we find the
+            // General Purpose Bit Flag.
+            // http://www.pkware.com/documents/casestudies/APPNOTE.TXT
+            RAFStream rafStream= new RAFStream(localRaf, entry.localHeaderRelOffset + 6);
             DataInputStream is = new DataInputStream(rafStream);
+            int gpbf = Short.reverseBytes(is.readShort());
+            if ((gpbf & ~ZipFile.GPBF_SUPPORTED_MASK) != 0) {
+                throw new ZipException("General Purpose Bit Flag bit(s) not supported, GPBF=" + gpbf);
+            }
+
+            // At position 28 we find the length of the extra data. In some cases
+            // this length differs from the one coming in the central header.
+            is.skipBytes(20);
             int localExtraLenOrWhatever = Short.reverseBytes(is.readShort());
             is.close();
 

Evervolv/android_libcore

Conflicts:
	luni/src/main/java/java/util/zip/ZipFile.java

luni/src/main/java/java/util/zip/ZipFile.java
@@ -69,7 +69,13 @@ public class ZipFile implements ZipConstants {
     static final int GPBF_UTF8_FLAG = 1 << 11;
 
     /**
-     * Open ZIP file for reading.
+     * Supported General Purpose Bit Flags Mask.
+     * Bit mask of supported GPBF bits.
+     */
+    static final int GPBF_SUPPORTED_MASK = GPBF_DATA_DESCRIPTOR_FLAG | GPBF_UTF8_FLAG;
+
+    /**
+     * Open zip file for reading.
      */
     public static final int OPEN_READ = 1;
 
@@ -243,11 +249,19 @@ public class ZipFile implements ZipConstants {
         RandomAccessFile raf = mRaf;
         synchronized (raf) {
             // We don't know the entry data's start position. All we have is the
-            // position of the entry's local header. At position 28 we find the
-            // length of the extra data. In some cases this length differs from
-            // the one coming in the central header.
-            RAFStream rafStream = new RAFStream(raf, entry.mLocalHeaderRelOffset + 28);
+            // position of the entry's local header. At position 6 we find the
+            // General Purpose Bit Flag.
+            // http://www.pkware.com/documents/casestudies/APPNOTE.TXT
+            RAFStream rafStream= new RAFStream(raf, entry.mLocalHeaderRelOffset + 6);
             DataInputStream is = new DataInputStream(rafStream);
+            int gpbf = Short.reverseBytes(is.readShort());
+            if ((gpbf & ~ZipFile.GPBF_SUPPORTED_MASK) != 0) {
+                throw new ZipException("Invalid General Purpose Bit Flag: " + gpbf);
+            }
+
+            // At position 28 we find the length of the extra data. In some cases
+            // this length differs from the one coming in the central header.
+            is.skipBytes(20);
             int localExtraLenOrWhatever = Short.reverseBytes(is.readShort());
             is.close();
 